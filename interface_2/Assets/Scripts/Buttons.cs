2decc4e349f28c20bebdc985dfb3693a057729ad
Dan Grover <dmgrover1@verizon.net>
2014-11-22 16:58:05 -0500

    Commented the Code
    Commented the code

--- interface_2/Assets/Scripts/Buttons.cs
+++ interface_2/Assets/Scripts/Buttons.cs
@@ -2,18 +2,30 @@
 using System.Collections;
 
 public class Buttons : MonoBehaviour {
+	//Screen Indicator
 	private int count = 0;
+
+	//Game Timer
 	private float timer = 100;
+
+	//Sizex and Sizey represent the size of the icons/images
 	private int sizex = 53;
 	private int sizey = 52;
+
+	//Icons with booleans that add them to the bowl when selected (maybe group these)
 	public Texture2D icon;
 	private bool ic1 = false;
 	public Texture2D icon2;
 	private bool ic2 = false;
 	public Texture2D icon3;
+
+	//Texture for the bowl
 	public Texture2D bowl;
+
+	//Allows for custom icons
 	public GUIStyle style;
 
+	//Updates the game in terms of player using the arrow keys to move screens or reset the game
 	void Update() {
 		if (count < 4) {
 			if (Input.GetKeyDown (KeyCode.RightArrow) || Input.GetKeyDown (KeyCode.D)) {
@@ -33,19 +45,25 @@ public class Buttons : MonoBehaviour {
 		timer -= Time.deltaTime;
 	}
 
+	//Resets the game
 	void reset () {
 		timer = 100;
 		ic1 = false;
 		ic2 = false;
 		}
-	
+
+	//Main GUI function, runs all the buttons
 	void OnGUI () {
-		GUI.Label (new Rect (Screen.width/2,0,100,50), "Count " + (count + 1));
+		//Screen indicator
+		GUI.Label (new Rect (Screen.width/2,0,100,50), "Screen " + (count + 1));
+
+		//Timer
 		GUI.Label (new Rect (Screen.width - 100,0,100,50), "Timer " + (int)timer);
+
+		//Bowl
 		GUI.Label (new Rect (Screen.width/2, Screen.height - 50,50,50), bowl, style);
-		saladGUI ();
-		screenGUI0 ();
-		screenGUI2 ();
+
+		//Determines whether a player uses the buttons to switch screens
 		if (count < 4) {
 			if (GUI.Button (new Rect (Screen.width - 100, Screen.height / 2 - 30, 100, 100), "Next Screen")) {
 					Camera.main.transform.Translate (new Vector3 (26, 0, 0));
@@ -58,8 +76,13 @@ public class Buttons : MonoBehaviour {
 				count -= 1;
 			}
 		}
+
+		saladGUI ();
+		screenGUI1 ();
+		screenGUI3 ();
 	}
 
+	//Determines what is in the salad/on top of the bowl
 	void saladGUI () {
 		if (ic1) {
 			GUI.Label (new Rect (Screen.width/2, Screen.height - 80,50,50), icon, style);
@@ -69,7 +92,8 @@ public class Buttons : MonoBehaviour {
 		}
 	}
 
-	void screenGUI0 () {
+	//Determines the icons on screen 1
+	void screenGUI1 () {
 	if (count == 0) {
 		if (GUI.Button (new Rect (50, 20, sizex, sizey),icon, style)) {
 			print ("you clicked the icon 1");
@@ -97,7 +121,8 @@ public class Buttons : MonoBehaviour {
 	}
 	}
 
-	void screenGUI2 () {
+	//Determines what happens on Screen 3
+	void screenGUI3 () {
 		if (count == 2) {
 			if (GUI.Button (new Rect (50, 20, sizex, sizey),icon, style)) {
 				print ("you clicked the icon 1");
