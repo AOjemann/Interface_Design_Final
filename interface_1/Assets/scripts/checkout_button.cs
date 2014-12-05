ebb7470a20923d8e889c4d8666f19fc7c5db6481
AOjemann <robintood@hotmail.com>
2014-12-04 19:51:24 -0500

    randomizer and checkout button changes

--- interface_1/Assets/scripts/checkout_button.cs
+++ interface_1/Assets/scripts/checkout_button.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class checkout_button : MonoBehaviour {
 
+	Buttons buttonScriptTMP;
+
+	int correctCount;
+	public int correctTillNextLevel;
+
 	// Use this for initialization
 	void Start () {
+
+		buttonScriptTMP = GameObject.FindWithTag ("buttons").GetComponent <Buttons> ();
+		correctCount = 0;
 	
 	}
 	
@@ -21,13 +29,22 @@ public class checkout_button : MonoBehaviour {
 	void checkout_button_compair (){
 		GameObject tmpPlayer;
 		tmpPlayer = GameObject.FindWithTag ("Player");
+		salad_randomizer tmpSalRand = tmpPlayer.GetComponent<salad_randomizer> ();
 		salad_compair sal_comp_tmp = tmpPlayer.GetComponent<salad_compair> ();
 		bool winTmp = sal_comp_tmp.compair_sal ();
 		if (winTmp) {
-			print("win");
+			print("Correct");
+			correctCount = correctCount + 1;
+			if (correctCount >= correctTillNextLevel){
+				buttonScriptTMP.winLevel();
+				tmpSalRand.setRandomSaladFromBook();
+				buttonScriptTMP.checkOutCorrectGUI();
+					}
 				}
 		if (!winTmp){
-			print ("NOT win");
+			print ("Not Correct, -10sec");
+			buttonScriptTMP.timeReduce();
+			buttonScriptTMP.checkOutIncorrectGUI();
 		}
 	}
 
---
+++ interface_1/Assets/scripts/salad_randomizer.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class salad_randomizer : MonoBehaviour {
+
+	//all salad recipies are atached to the player as instances of salad strucs that have been compleeted
+	//in the inspector, They are always attached after the example salad and the player salad
+
+	salad_struc[] posibleSal;
+	int randomSalIndex;
+	Buttons buttonSTMP;
+
+	public int number_of_recipies;
+
+	salad_struc exSal;
+	salad_struc randomSal;
+	salad_struc playerSal;
+
+	// Use this for initialization
+	void Start () {
+	
+		posibleSal =  this.GetComponents<salad_struc> ();
+		buttonSTMP = GameObject.FindWithTag ("buttons").GetComponent<Buttons> ();
+		exSal = posibleSal [0];
+		playerSal = posibleSal [1];
+		setRandomSaladFromBook ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	//sets ex salad equal to a random salad from the recipie book
+	public void setRandomSaladFromBook(){
+
+		randomSalIndex = Random.Range (2, number_of_recipies + 1);
+		randomSal = posibleSal[randomSalIndex];
+
+		exSal.clear_sal ();
+		exSal.add_base (randomSal.get_base());
+		exSal.add_dress (randomSal.get_dress());
+		exSal.add_prot (randomSal.get_prot());
+		exSal.add_top (randomSal.get_top1());
+		exSal.add_top (randomSal.get_top2());
+		exSal.add_top (randomSal.get_top3());
+
+		print (randomSal.sal_name);
+		buttonSTMP.nextOrder (randomSal.sal_name);
+
+		playerSal.clear_sal ();
+
+
+		}
+
+}
